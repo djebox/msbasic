a07fcacb2e3e5124cdcb1690c1c84d7dd2a2cf12
Fahrid DJEBBARI <fahrid.djebbari@gmail.com>
2026-05-17 17:53:46 +0200

    putchar op for msbasic

--- p6502_extra.s
+++ p6502_extra.s
@@ -6,20 +6,39 @@ MMIO_SPIN := $FFE1
 MMIO_A := $FFE4
 MMIO_UART_TX := $FFEF
 
-OP_GETCHAR := $8
+OP_PUTCHAR := $8
+OP_GETCHAR := $9
 
-; Modifies flags
+; Modifies flags, A, X
 MONCOUT:
-    sta MMIO_UART_TX
+    pha
+    phx
+
+    ;sta MMIO_UART_TX
+    ;lda     #$FF
+@txdelay:
+    ;dec
+    ;bne     @txdelay
+
+    sta MMIO_A
+    lda #OP_PUTCHAR
+    sta MMIO_OP
+    jsr MMIO_SPIN   ; modifies A & X
+
+    plx
+    pla
     rts
 
 ; Modifies flags, A, X
 MONRDKEY:
     phx
+
     lda #OP_GETCHAR
     sta MMIO_OP
     jsr MMIO_SPIN   ; modifies A & X
-    jsr MONCOUT
+
+    jsr MONCOUT     ; echo
+    
     plx
     rts
 
